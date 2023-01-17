704704b91eff0167639a9e4d6d4adae0849bd649
jonot <josephtessitore@protonmail.com>
2023-01-17 16:44:08 -0500

    Try to get pwa working

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,26 @@ import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    solidPlugin(), 
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 3000,
   },
