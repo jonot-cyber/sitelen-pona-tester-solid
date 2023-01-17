cb3129eca67aeecc6bdcd6eec81ebcf70101894f
jonot <josephtessitore@protonmail.com>
2023-01-17 16:07:22 -0500

    Add event

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Component, createSignal, For } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
 import wordlist from './wordlist';
+import WordButton from './WordButton';
 
 function randomWord(excluding?: string): string {
   const availableWords = wordlist.filter(word => word != excluding)
@@ -29,6 +30,16 @@ function shuffleArray(list: string[]) {
 const App: Component = () => {
   const [word, setWord] = createSignal(randomWord());
 
+  function handleAnswer(guess: string) {
+    if (guess === word()) {
+      alert("Correct!")
+    } else {
+      alert(`Incorrect! The correct answer was ${word()}`)
+    }
+
+    setWord(randomWord())
+  }
+
   return (
     <div class="h-screen flex flex-col">
       <div class="grid justify-center content-center grow select-none">
@@ -36,10 +47,7 @@ const App: Component = () => {
       </div>
       <div class="flex w-full">
         <For each={generateList(word(), 4)}>{(word, number) =>
-          <button class="border-4 border-black flex p-2 m-2 flex-1">
-            <span class="text-center text-4xl grow">{word}</span>
-            <span>{number() + 1}</span>
-          </button>}
+          <WordButton word={word} number={number()} answer={handleAnswer} />}
         </For>
       </div>
     </div>
---
+++ src/WordButton.tsx
@@ -0,0 +1,26 @@
+import { Component, JSX, onCleanup, onMount } from "solid-js";
+
+const WordButton: Component<{word: string, number: number, answer: (guess: string) => void}> = (props) => {
+    function keyListener(ev: KeyboardEvent) {
+        if (ev.key === (props.number + 1).toString()) {
+            props.answer(props.word)
+        }
+    }
+
+    onMount(async () => {
+        window.addEventListener("keypress", keyListener)
+    })
+
+    onCleanup(async () => {
+        window.removeEventListener("keypress", keyListener)
+    })
+
+    return (
+        <button onclick={() => {props.answer(props.word)}} class="border-4 border-black flex p-2 m-2 flex-1">
+            <span class="text-center text-4xl grow">{props.word}</span>
+            <span>{props.number + 1}</span>
+        </button>
+    )
+}
+
+export default WordButton
