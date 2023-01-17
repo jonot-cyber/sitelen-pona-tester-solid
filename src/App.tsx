b3934fdcefc314904b269a61df082182be17d1d9
jonot <josephtessitore@protonmail.com>
2023-01-17 15:41:12 -0500

    UI Prototyping

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import type { Component } from 'solid-js';
+import { Component, For } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="h-screen flex flex-col">
+      <div class="grid justify-center content-center grow select-none">
+        <p class="font-sitelen text-[80vh] leading-none">toki</p>
+      </div>
+      <div class="flex w-full">
+        <For each={["toki", "pona", "sitelen", "mu"]}>{(word, number) =>
+          <button class="border-4 border-black flex p-2 m-2 flex-1">
+            <span class="text-center text-4xl grow">{word}</span>
+            <span>{number() + 1}</span>
+          </button>}
+        </For>
+      </div>
     </div>
   );
 };
